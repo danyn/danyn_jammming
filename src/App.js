a5d9d5ad22469ed9e4be7408b4e4bf18e1d1426b
danyn <danyn.com@gmail.com>
2018-03-13 17:37:37 -0700

    initial commitall components are working - no props or state

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import {SearchBar} from './components/SearchBar/SearchBar';
+import {SearchResults} from './components/SearchResults/SearchResults';
+import {Playlist} from './components/Playlist/Playlist';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar/>
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>{/* div.App */}
+      </div>
+    );// return
+  }// render
+}// App
+
+export default App;
